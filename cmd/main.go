beb317962fb55c3ed8c41ddc867a2dc6821b6751
G8-1695 <victor>
2024-07-11 13:33:11 -0300

    feat: adapter, greet, cmd

---
+++ cmd/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"context"
+	"log"
+
+	"github.com/viquitorreis/my-grpc-go-client/internal/adapter/hello"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+func main() {
+	log.SetFlags(0)
+	log.SetOutput(&logWriter{})
+
+	// Create a new gRPC client
+	var opts []grpc.DialOption
+	opts = append(opts, grpc.WithTransportCredentials(insecure.NewCredentials()))
+
+	// Connect to gRPC server
+	conn, err := grpc.NewClient("localhost:9090", opts...)
+	if err != nil {
+		log.Fatalln("Erro ao conectar com o servidor gRPC, err:", err)
+	}
+	defer conn.Close()
+
+	// Create a new adapter
+	helloAdapter, err := hello.NewHelloAdapter(conn)
+	if err != nil {
+		log.Fatal("Erro ao criar o adapter de hello, err:", err)
+	}
+
+	runSayHello(helloAdapter, "Víctor reis")
+}
+
+func runSayHello(adapter *hello.HelloAdapter, name string) {
+	greet, err := adapter.SayHello(context.Background(), name)
+	if err != nil {
+		log.Fatalln("Erro ao chamar o serviço de hello, err:", err)
+	}
+
+	log.Println("Resposta do serviço de hello:", greet.Message)
+}
--- internal/adapter/hello/hello_adapter.go
+++ internal/adapter/hello/hello_adapter.go
@@ -1,6 +1,9 @@
 package hello
 
 import (
+	"context"
+	"log"
+
 	"github.com/viquitorreis/my-grpc-go-client/internal/port"
 	"github.com/viquitorreis/my-grpc-proto/protogen/go/hello"
 	"google.golang.org/grpc"
@@ -14,4 +17,18 @@ func NewHelloAdapter(conn *grpc.ClientConn) (*HelloAdapter, error) {
 	return &HelloAdapter{
 		helloClient: hello.NewHelloServiceClient(conn),
 	}, nil
+}
+
+func (a *HelloAdapter) SayHello(ctx context.Context, name string) (*hello.HelloResponse, error) {
+	helloRequest := &hello.HelloRequest{
+		Name: name,
+	}
+
+	greet, err := a.helloClient.SayHello(ctx, helloRequest)
+	if err != nil {
+		log.Fatal("Erro ao chamar o serviço de hello, err:", err)
+	}
+
+	// greet = hello response
+	return greet, nil
 }
